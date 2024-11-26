04b035f446e60b6954d9191c5e61902090feec68
max <maxvin13a@gmail.com>
2024-11-26 22:42:53 +0100

    fost

---
+++ main.py
@@ -0,0 +1,15 @@
+from produkt import produkt
+
+amazon = produkt(name = "pc", cost = 10000, willdescribe = "SYSTEMTREFF Basic Gaming Komplett PC Set AMD Ryzen 5 PRO 4650G 6x4.2GHz | AMD RX Vega 7 4K HDMI DX12 | 512GB M.2 NVMe + 1TB HDD | 16GB DDR4 RAM | WLAN Desktop Paket Computer für Gamer, Gaming")
+n = amazon
+print("cart")
+
+
+otto = produkt(name = "ps 5", cost = 4289, willdescribe = "Consola PS5 ")
+e = otto
+
+steam = produkt(name = " God of War Ragnarok", cost = 79, willdescribe = "")
+r = steam
+
+basket = produkt()
+m = basket
---
+++ produkt.py
@@ -0,0 +1,82 @@
+class produkt:
+
+    print("ok or no")
+    n = int(input())
+    if n % 2 == 0:
+        print('ok')
+        n = n - 10000
+    if n == print('ok'):
+     n == 'm'
+    else:
+        print('no')
+        n = n - 0
+        n == 'm'
+
+        print("ok or no")
+        e = int(input())
+        if e % 2 == 0:
+            print('ok')
+            e = e - 10000
+        if e == print('ok'):
+            e == 'm'
+        else:
+            print('no')
+            e = e - 0
+            if e == print('no'):
+                e / 'm'
+
+            print("ok or no")
+            r = int(input())
+            if r % 2 == 0:
+                print('ok')
+                r = r - 79
+                if r == print('ok'):
+                    r == 'm'
+            else:
+                print('no')
+                r = r - 0
+                if r == print('no'):
+                    r / 'm'
+
+            print("ok or no")
+            m = int(input())
+            print("basket:")
+            m == n
+            m == e
+            m == r
+            basket = __import__("pc ps 5  God of War Ragnarok ")
+
+        m == n
+        m / e
+        m / r
+        basket = __import__("pc  ")
+
+        m == n
+        m / e
+        m == r
+        basket = __import__("pc  God of War Ragnarok ")
+
+        m == n
+        m == e
+        m / r
+        basket = __import__("pc  ps 5  ")
+
+        m / n
+        m == e
+        m == r
+        basket = __import__(" ps 5  God of War Ragnarok ")
+
+        m / n
+        m == e
+        m / r
+        basket = __import__(" ps 5  ")
+
+        m / n
+        m / e
+        m == r
+        basket = __import__("  God of War Ragnarok. ")
+
+        m / n
+        m / e
+        m / r
+        basket = __import__("")
